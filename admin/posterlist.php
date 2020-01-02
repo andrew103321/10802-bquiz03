2403d525d206147361ba84cdd99dafcd92240294
andrew103321 <andrew103321@gmail.com>
2020-01-02 16:39:08 +0800

    乙級03後臺

---
+++ admin/posterlist.php
@@ -0,0 +1,33 @@
+<?php
+        include_once "../base.php";
+
+                $pos = all("poster",[],"order by rank");
+                foreach($pos as $k => $p){
+                    // 計算上一筆
+                    $prev = ($k!=0)?$pos[$k-1]['id']:$p['id'];
+                    // 計算下一筆
+                    // 計算陣列比數  count
+                    $next = ($k!=(count($pos)-1))?$pos[$k+1]['id']:$p['id'];
+            ?>
+            <ul class="row">
+                <li><img src="./poster/<?=$p['poster'];?>" alt=""></li>
+                <li><input type="text" name="name[]" value="<?=$p['name'];?>"></li>
+                <li>                                  
+                                                   <!-- id=v現在頁."-".交換頁 -->
+                    <input type="button" value="往上" id=<?=$p['id']."-".$prev?>>
+                    <input type="button" value="往下" id=<?=$p['id']."-".$next?>>
+                </li>
+                <li>
+                    <input type="checkbox" name="sh[]" id="<?=$p['id'];?>" <?=($p['sh']==1)?'checked':"";?>>顯示
+                    <input type="checkbox" name="del[]" id="<?=$p['id'];?>">刪除
+                    <select name="ani[]" >
+                            <option value="1" <?=($p['ani']==1)?"selected":"";?>>淡入淡出</option>
+                            <option value="2" <?=($p['ani']==2)?"selected":"";?>>滑入滑出</option>
+                            <option value="3" <?=($p['ani']==3)?"selected":"";?>>縮放</option>
+                    </select>
+                </li>
+                <input type="hidden" name="id[]" value="<?=$p['id'];?>">
+            </ul>
+            <?php
+            }
+            ?>
---
+++ base.php
@@ -0,0 +1,100 @@
+<?php
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=db03_1";
+    $pdo = new PDO($dsn,"root","");
+    session_start(); // *
+
+
+
+    function find($table,...$arg){
+        global $pdo;
+        
+        $sql = "select * from $table where " ;
+
+        if(is_array($arg[0])){
+            foreach($arg[0] as  $key => $value ){
+                $tmp[] =  sprintf("`%s`='%s'",$key,$value);
+            }
+                $sql = $sql .implode(" && ",$tmp);
+        }else{
+                $sql = $sql . "`id`='".$arg[0]."'" ;  /**/ 
+        }
+            // echo $sql ;
+            return $pdo->query($sql)->fetch(PDO::FETCH_ASSOC); /**/
+    }
+            // print_r(find("total",1)) ;
+
+    function all($table,...$arg){
+        global $pdo;
+
+        $sql = "select * from $table  ";
+
+        if(!empty($arg[0])){
+            foreach($arg[0] as $key=>$value){    // *
+                $tmp[] = sprintf("`%s`='%s'",$key,$value);
+            }
+            $sql = $sql ."where". implode(" && ",$tmp);   // *
+          
+        }
+
+        if(!empty($arg[1])){
+            $sql = $sql . $arg[1];     
+        }
+
+       return $pdo->query($sql)->fetchALL();
+    }
+
+        // print_r(all("total",["total"=>2],"limit 2"));   //*//*
+
+
+        function del($table,...$arg){
+            global $pdo;
+            $sql = "delete from $table where ";
+            if(is_array($arg[0])){
+                foreach($arg[0] as $key=>$value){
+                    $tmp[] = sprintf("`%s`='%s'",$key,$value);
+                 
+                }
+                    $sql = $sql . implode(" && " ,$tmp);
+            }else{
+                $sql = $sql . "`id`='".$arg[0]."'"; //*
+             
+            }
+          
+
+        }
+
+       function save($table,$data){
+            global $pdo;
+            if(!empty($data['id'])){
+                //up
+                foreach($data as $key=>$value){
+                    $tmp[] = sprintf("`%s`='%s'",$key,$value);
+                }
+                $sql = "update $table set ".implode(",",$tmp)."  where `id`='".$data['id']."' " ;
+          
+            }else{
+                //in
+                    $key = array_keys($data);
+                    $key_str = "`".implode("`,`",$key)."`";
+                    $data_str = "'".implode("','",$data)."'";
+
+                    $sql = "insert into $table ($key_str) value($data_str)";
+            }
+            // echo $sql;
+            return $pdo->exec($sql);
+       }
+
+
+        function to($path){
+            header("location:".$path);   //*
+        }
+
+
+        function q($sql){
+            global $pdo;
+            return $pdo->query($sql)->fetchALL();
+        }
+     
+
+
+?>
